7027f4a508d2cdfe3ed7b7631eb17b675c8279a1
A5ho9999 <a5ho9999.gamer@gmail.com>
2024-06-17 15:00:10 +0930

    Remove unnecessary files and set + Update to 1.21

--- src/main/java/de/guntram/mcmod/randomblockplacement/RandomBlockPlacement.java
+++ src/main/java/de/guntram/mcmod/randomblockplacement/RandomBlockPlacement.java
@@ -1,15 +1,7 @@
 package de.guntram.mcmod.randomblockplacement;
 
-import com.mojang.brigadier.CommandDispatcher;
-import static com.mojang.brigadier.arguments.IntegerArgumentType.getInteger;
-import static com.mojang.brigadier.arguments.IntegerArgumentType.integer;
-import de.guntram.mcmod.crowdintranslate.CrowdinTranslate;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
-import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
-import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
-import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
@@ -22,35 +14,38 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.text.Text;
+
+import static com.mojang.brigadier.arguments.IntegerArgumentType.getInteger;
+import static com.mojang.brigadier.arguments.IntegerArgumentType.integer;
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_R;
 
-public class RandomBlockPlacement implements ClientModInitializer
-{
-    static final String MODID="randomblockplacement";
-    static final String MODNAME="RandomBlockPlacement";
+public class RandomBlockPlacement implements ClientModInitializer {
+    static final String MODID = "randomblockplacement";
+    static final String MODNAME = "RandomBlockPlacement";
 
     private static RandomBlockPlacement instance;
     private boolean isActive;
     private int minSlot, maxSlot;
     private KeyBinding onOff;
-    
+
     @Override
     public void onInitializeClient() {
-        CrowdinTranslate.downloadTranslations(MODID);
         instance = this;
         setKeyBindings();
         registerCommands();
-        isActive=false;
-        minSlot=0;
-        maxSlot=PlayerInventory.getHotbarSize() - 1;
+        isActive = false;
+        minSlot = 0;
+        maxSlot = PlayerInventory.getHotbarSize() - 1;
     }
-    
+
     private void setKeyBindings() {
         final String category = "key.categories.randomblockplacement";
         KeyBindingHelper.registerKeyBinding(onOff = new KeyBinding("key.randomblockplacement.toggle", InputUtil.Type.KEYSYM, GLFW_KEY_R, category));
-        ClientTickEvents.END_CLIENT_TICK.register(e->processKeyBinds());
+        ClientTickEvents.END_CLIENT_TICK.register(e -> processKeyBinds());
     }
-    
+
     private void processKeyBinds() {
         if (onOff.wasPressed()) {
             if (isActive) {
@@ -70,22 +65,22 @@ public class RandomBlockPlacement implements ClientModInitializer
         if (player != null) {
             player.sendMessage(Text.translatable("msg.inactive"), false);
         }
-        isActive=false;
+        isActive = false;
     }
 
     public void setActive(int first, int last) {
-        minSlot = first-1;
-        maxSlot = last-1;
+        minSlot = first - 1;
+        maxSlot = last - 1;
         if (maxSlot >= PlayerInventory.getHotbarSize()) {
             maxSlot = PlayerInventory.getHotbarSize() - 1;
-        }        
+        }
         setActive();
     }
-    
+
     public void setActive() {
         ClientPlayerEntity player = MinecraftClient.getInstance().player;
         if (player != null) {
-            player.sendMessage(Text.translatable("msg.active", minSlot+1, maxSlot+1), false);
+            player.sendMessage(Text.translatable("msg.active", minSlot + 1, maxSlot + 1), false);
         }
         isActive = true;
     }
@@ -96,27 +91,27 @@ public class RandomBlockPlacement implements ClientModInitializer
             return;
         }
         PlayerInventory inventory = player.getInventory();
-        int index=inventory.selectedSlot;
+        int index = inventory.selectedSlot;
         if (maxSlot >= PlayerInventory.getHotbarSize()) {
             maxSlot = PlayerInventory.getHotbarSize() - 1;
         }
-        if (isActive && index>=minSlot && index<=maxSlot
-            && ( inventory.getStack(index).getItem() == Items.AIR
+        if (isActive && index >= minSlot && index <= maxSlot
+                && (inventory.getStack(index).getItem() == Items.AIR
                 || inventory.getStack(index).getItem() instanceof BlockItem)) {
-            int totalBlocks=0;
-            for (int i=minSlot; i<=maxSlot; i++) {
-                totalBlocks+=getBlockCount(inventory, i);
+            int totalBlocks = 0;
+            for (int i = minSlot; i <= maxSlot; i++) {
+                totalBlocks += getBlockCount(inventory, i);
             }
-            int targetCount=(int) (Math.random()*totalBlocks);
-            int targetSlot=minSlot;
-            while (targetSlot<maxSlot && targetCount>=getBlockCount(inventory, targetSlot)) {
-                targetCount-=getBlockCount(inventory, targetSlot);
+            int targetCount = (int) (Math.random() * totalBlocks);
+            int targetSlot = minSlot;
+            while (targetSlot < maxSlot && targetCount >= getBlockCount(inventory, targetSlot)) {
+                targetCount -= getBlockCount(inventory, targetSlot);
                 targetSlot++;
             }
-            inventory.selectedSlot=targetSlot;
+            inventory.selectedSlot = targetSlot;
         }
     }
-    
+
     private int getBlockCount(PlayerInventory inventory, int targetSlot) {
         ItemStack stack = inventory.getStack(targetSlot);
         Item item = stack.getItem();
@@ -126,26 +121,26 @@ public class RandomBlockPlacement implements ClientModInitializer
             return 0;
         }
     }
-    
+
     public void registerCommands() {
         int size = PlayerInventory.getHotbarSize();
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
-        	dispatcher.register(
-                literal("rblock")
-                    .then(
-                        literal("off").executes(c->{
-                            instance.setInactive();
-                            return 1;
-                        })
-                    )
-                    .then(
-                        argument("b1", integer(1, size)).then (
-                            argument("b2", integer(1, size)).executes(c->{
-                                instance.setActive(getInteger(c, "b1"), getInteger(c, "b2"));
-                                return 1;
-                            })
-                        )
-                    )
+            dispatcher.register(
+                    literal("rblock")
+                            .then(
+                                    literal("off").executes(c -> {
+                                        instance.setInactive();
+                                        return 1;
+                                    })
+                            )
+                            .then(
+                                    argument("b1", integer(1, size)).then(
+                                            argument("b2", integer(1, size)).executes(c -> {
+                                                instance.setActive(getInteger(c, "b1"), getInteger(c, "b2"));
+                                                return 1;
+                                            })
+                                    )
+                            )
             );
         });
     }
